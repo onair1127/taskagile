fe3aaf425ef378b30a11b9661d616aea879bed66
onair1127 <onair1127@naver.com>
2020-08-08 23:11:22 +0900

    combine FE and BE

--- front-end/tests/e2e/specs/login.e2e.js
+++ front-end/tests/e2e/specs/login.e2e.js
@@ -1,8 +1,8 @@
 // For authoring Nightwatch tests, see
 // https://nightwatchjs.org/guide
 
-module.exports = {  // node.js에서의 모듈 정의, 나이트왓치 테스트를 여기에 작성
-  'login test': function(browser) {
+module.exports = {
+  'login test': function (browser) {
     browser
       .url(process.env.VUE_DEV_SERVER_URL + 'login')
       .waitForElementVisible('#app', 5000)
